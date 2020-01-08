b8b1a1ca75e9eb8084d900b8d2170b4626e18081
Student <example@example.com>
2020-01-08 10:20:54 -0800

    add delete, clear, find functions and tests

--- lib/city.rb
+++ lib/city.rb
@@ -1,15 +1,24 @@
 class City
   attr_accessor :name, :id
 
+  # create a city
   def initialize(attributes)
     @name = attributes.fetch(:name) || nil
     @id = attributes.fetch(:id, nil)
   end
 
+  # ensure two cities are the same if they have the same name
   def ==(city_to_compare)
     self.name() == city_to_compare.name()
   end
+  #
+  # save a city
+  def save
+    result = DB.exec("INSERT INTO cities (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
 
+  # read all cities
   def self.all
     returned_cities = DB.exec('SELECT * FROM cities;')
     cities = []
@@ -21,11 +30,29 @@ class City
     cities
   end
 
-  def save
-    result = DB.exec("INSERT INTO cities (name) VALUES ('#{@name}') RETURNING id;")
-    @id = result.first().fetch("id").to_i
+  # update a city
+  def update(name)
+    @name = name
+    DB.exec("UPDATE cities SET name = '#{@name}' WHERE id = #{@id};")
   end
 
+  #delete a city
+  def delete
+    DB.exec("DELETE FROM cities WHERE id = #{@id};")
+  end
 
+  #clear city database
+  def self.clear
+    DB.exec("DELETE FROM cities *;")
 
+  end
+
+  def self.find(id)
+    city = DB.exec("SELECT * FROM cities WHERE id = #{id};").first
+    name = city.fetch("name")
+    id = city.fetch("id").to_i
+    City.new({:name => name, :id => id})
+  end
+
+  
 end
--- spec/city_spec.rb
+++ spec/city_spec.rb
@@ -16,15 +16,56 @@ describe('.#City') do
     end
   end
 
-  describe('#save') do
-  it("saves an city") do
-    city = City.new({:name => "Chicago", :id => nil}) # nil added as second argument
-    city.save()
-    city2 = City.new({:name => "Portland", :id => nil}) # nil added as second argument
-    city2.save()
-    expect(City.all).to(eq([city, city2]))
-  end
-end
 
+  describe('#save') do
+    it("saves an city") do
+      city = City.new({:name => "Chicago", :id => nil}) # nil added as second argument
+      city.save()
+      city2 = City.new({:name => "Portland", :id => nil}) # nil added as second argument
+      city2.save()
+      expect(City.all).to(eq([city, city2]))
+    end
+  end
+
+  describe('.clear') do
+    it("clears all cities") do
+      album = City.new({:name => "Chicago", :id => nil})
+      album.save()
+      album2 = City.new({:name => "Portland", :id => nil})
+      album2.save()
+      City.clear()
+      expect(City.all).to(eq([]))
+    end
+  end
+
+  describe('#update') do
+    it("updates an city by id") do
+      city = City.new({:name => "Chicago", :id => nil})
+      city.save()
+      city.update("Portland")
+      expect(city.name).to(eq("Portland"))
+    end
+  end
+
+  describe('#delete') do
+    it("deletes an city") do
+      album = City.new({:name => "Chicago", :id => nil})
+      album.save()
+      album2 = City.new({:name => "Portland", :id => nil})
+      album2.save()
+      album.delete()
+      expect(City.all).to(eq([album2]))
+    end
+  end
+
+  describe('.find') do
+    it("finds a city by id") do
+      city = City.new({:name => "Chicago", :id => nil})
+      city.save()
+      city2 = City.new({:name => "Portland", :id => nil})
+      city2.save()
+      expect(City.find(city.id)).to(eq(city))
+    end
+  end
 
 end
